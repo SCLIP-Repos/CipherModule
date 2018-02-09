5edc4b805a841c8e3b54a9715f649446475a5fd5
Ryu-ji <ryuji.mail.g@gmail.com>
2018-02-09 21:12:13 +0900

    Write RSA.cs

--- src/PublicKeyEncryptor/RSA.cs
+++ src/PublicKeyEncryptor/RSA.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CipherModule.PublicKeyEncryptor
+namespace CipherModule
 {
-    class RSA:Interfaces.Out
+    public class RSA:Interfaces.Out
     {
 
         private string print, err;
@@ -17,22 +17,47 @@ namespace CipherModule.PublicKeyEncryptor
         
 
         public RSA() => _rsa = new RSACryptoServiceProvider();
+        
 
-
-        public void SET()
+        public void Encrypt(string Source,string Key)
         {
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                try
+                {
+                    rsa.FromXmlString(Key);
 
+                    print = Convert.ToBase64String(rsa.Encrypt(Encoding.UTF8.GetBytes(Source), false));
+                }
+                catch(Exception ex)
+                {
+                    err = ex.ToString();
+                }
+            }
         }
 
-
-        public void Encrypt()
+        public void Decrypt(string Source,string Key)
         {
+            var Tmp = Convert.FromBase64String(Source);
 
-        }
+            Key key = new RSA.Key();
 
-        public void Decrypt()
-        {
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                try
+                {
+                    rsa.FromXmlString(Key);
+                    
+                    byte[] SourceBytes = rsa.Decrypt(Tmp, false);
 
+                    print = Encoding.UTF8.GetString(SourceBytes);
+                }
+                catch (Exception ex)
+                {
+                    err = ex.ToString();
+                }
+
+            }
         }
 
         public void Close() => _rsa.Dispose();
@@ -59,14 +84,43 @@ namespace CipherModule.PublicKeyEncryptor
         }
 
 
+
         public class Key
         {
 
-            public void CreateKeyPair()
+            public void CreateKeyPair(out string PublicKey,out string PrivateKey,string ContainerName)
+            {
+                CspParameters cspParameters = new CspParameters();
+
+                cspParameters.KeyContainerName = ContainerName;
+
+                using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cspParameters))
+                {
+                    PublicKey = rsa.ToXmlString(false);
+
+                    PrivateKey = rsa.ToXmlString(true);
+                }
+            }
+
+            public void CreateKeyPair(out string PublicKey,out string PrivateKey,out string ContainerName)
             {
 
+                ContainerName = Guid.NewGuid().ToString("N").Substring(0, 20);
+
+
+                CreateKeyPair(out PublicKey, out PrivateKey, ContainerName);
             }
-            
+
+
+
+            internal bool TypeOfElementToDecryptChecker(string Element)
+            {
+                if (Element.Remove("<RSAKeyValue>".Length) == "<RSAKeyValue>")
+                    return true;
+
+                return false;
+            }
+
         }
     }
 }
