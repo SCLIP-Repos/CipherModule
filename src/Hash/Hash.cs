7a32cc60f29146eab2f41795f326bc6c82c7c95e
Ryu-ji <ryuji.mail.g@gmail.com>
2018-02-09 20:38:55 +0900

    New Files
    Add Hash.cs and RSA.cs

---
+++ src/Hash/Hash.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace CipherModule
+{
+    public class SHA
+    {
+        public static string MD5(string Origin)
+        {
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                var Tmp = md5.ComputeHash(Encoding.UTF8.GetBytes(Origin));
+
+                StringBuilder stringBuilder = new StringBuilder();
+
+
+
+                for (int i = 0; i < Tmp.Length; i++)
+                {
+                    stringBuilder.AppendFormat("{0:x2}", Tmp[i]);
+                }
+
+                return stringBuilder.ToString();
+            }
+
+        }
+
+        public static string SHA_1(string Origin)
+        {
+            using (SHA1CryptoServiceProvider sha1 = new SHA1CryptoServiceProvider())
+            {
+                var Tmp = sha1.ComputeHash(Encoding.UTF8.GetBytes(Origin));
+
+                StringBuilder stringBuilder = new StringBuilder();
+
+
+
+                for (int i = 0; i < Tmp.Length; i++)
+                {
+                    stringBuilder.AppendFormat("{0:x2}", Tmp[i]);
+                }
+
+                return stringBuilder.ToString();
+            }
+        }
+
+
+        public static string SHA_256(string Origin)
+        {
+            using (SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider())
+            {
+                var Tmp = sha256.ComputeHash(Encoding.UTF8.GetBytes(Origin));
+
+                StringBuilder stringBuilder = new StringBuilder();
+
+
+
+                for (int i = 0; i < Tmp.Length; i++)
+                {
+                    stringBuilder.AppendFormat("{0:x2}", Tmp[i]);
+                }
+
+                return stringBuilder.ToString();
+            }
+        }
+
+        public static string SHA_384(string Origin)
+        {
+            using (SHA384CryptoServiceProvider sha384 = new SHA384CryptoServiceProvider())
+            {
+                var Tmp = sha384.ComputeHash(Encoding.UTF8.GetBytes(Origin));
+
+
+                StringBuilder stringBuilder = new StringBuilder();
+
+
+
+                for (int i = 0; i < Tmp.Length; i++)
+                {
+                    stringBuilder.AppendFormat("{0:x2}", Tmp[i]);
+                }
+
+                return stringBuilder.ToString();
+            }
+
+        }
+    }
+}
---
+++ src/PublicKeyEncryptor/RSA.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherModule.PublicKeyEncryptor
+{
+    class RSA:Interfaces.Out
+    {
+
+        private string print, err;
+
+        private RSACryptoServiceProvider _rsa;
+
+        
+
+        public RSA() => _rsa = new RSACryptoServiceProvider();
+
+
+        public void SET()
+        {
+
+        }
+
+
+        public void Encrypt()
+        {
+
+        }
+
+        public void Decrypt()
+        {
+
+        }
+
+        public void Close() => _rsa.Dispose();
+
+        public string Print()
+        {
+            return print;
+        }
+
+        public void SetPrint(string value)
+        {
+            SetPrint(print);
+        }
+
+
+        public string Err()
+        {
+            return err;
+        }
+
+        public void SetErr(string value)
+        {
+            SetErr(err);
+        }
+
+
+        public class Key
+        {
+
+            public void CreateKeyPair()
+            {
+
+            }
+            
+        }
+    }
+}
