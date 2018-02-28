18e7ee5bc546d5a7b6390ace84545b9e83d62a01
Ryu-ji <ryuji.mail.g@gmail.com>
2018-02-28 23:36:05 +0900

    add hmac file

---
+++ src/Hash/HMAC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace CipherModule
+{
+    class HMAC
+    {
+        /*
+        public static string SHA1(string Origin,string Key)
+        {
+            using (HMACSHA256 hmac = new HMACSHA256())
+            {
+               
+            }
+
+            return "";
+        }
+        */
+    }
+}
